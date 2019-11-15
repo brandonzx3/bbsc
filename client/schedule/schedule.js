dc62ef0dbd7f228ee8ca2316958a9d99cc2ce65c
Low qualtiy JPEG of biggie chesse <btherr2003@gmail.com>
2019-11-15 09:36:29 -0600

    fixed issues with status and added ability to change start time

--- client/schedule/schedule.js
+++ client/schedule/schedule.js
@@ -13,28 +13,31 @@ window.onload = function() {
         return seconds + " seconds";
     }
 
+
+    var schedule_times = [
+        [24600, 28200], //6:50 7:50
+        [28800, 31800], //8:00 8:50
+        [32100, 35100], //8:55 9:45
+        [35400, 37500], //9:50 10:25
+        [37800, 40800], //10:30 11:20
+        [41100, 44100], //11:25 12:15
+        [44400, 47400], //12:20 1:10
+        [47700, 50700], //1:15 2:05
+        [51000, 54000] //2:10 3:00
+    ]
+
+    let time_offset = -4;
+    let schedule_start_time = 1;
+
     function update_status() {
         const date = new Date();
         let ssm = (date.getHours() * 3600) + (date.getMinutes() * 60) + date.getSeconds();
-
-        var schedule_times = [
-            [24600, 28200], //6:50 7:50
-            [28800, 31800], //8:00 8:50
-            [32100, 35100], //8:55 9:45
-            [35400, 37500], //9:50 10:25
-            [37800, 40800], //10:30 11:20
-            [41100, 44100], //11:25 12:15
-            [44400, 47400], //12:20 1:10
-            [47700, 50700], //1:15 2:05
-            [51000, 54000] //2:10 3:00
-        ]
-        let time_offset = -4;
-
+        
         ssm += time_offset;
         //alert(ssm);
-        if(ssm < schedule_times[0]) status.innerHTML = `Status: School starts in ${elapsed_from_seconds(schedule_times[0][0] - ssm)}`;
+        if(ssm < schedule_times[schedule_start_time][0]) status.innerHTML = `Status: School starts in ${elapsed_from_seconds(schedule_times[schedule_start_time][0] - ssm)}`;
         
-        for (let i = 0; i < schedule_times.length; i++) {
+        for (let i = schedule_start_time; i < schedule_times.length; i++) {
             //In class
             if (schedule_times[i][0] <= ssm && ssm < schedule_times[i][1])
             status.innerHTML = `Status: In class. Class ends in ${elapsed_from_seconds(schedule_times[i][1] - ssm)}`;
@@ -45,7 +48,7 @@ window.onload = function() {
                 status.innerHTML = `Status: Heading to Class. Class starts in ${elapsed_from_seconds(schedule_times[ip][0] - ssm)}`;
             } 
         }
-        if(ssm > schedule_times[8][1]) status.innerHTML = "after school";
+        if(ssm > schedule_times[8][1]) status.innerHTML = "Status: after school";
     }
     setInterval(function(){ update_status(); }, 500);
 };
