e5f50c933a57405b0091b934570b102ca1f84aae
BuildTools <unconfigured@null.spigotmc.org>
2019-11-20 12:47:57 -0600

    Note Saving/Loading

--- client/notes/notes.js
+++ client/notes/notes.js
@@ -1,15 +1,39 @@
 window.onload = function() {
-    const content = document.querySelector("#content");
+  let saveNote = document.querySelector("#saveNote");
+  let loadNote = document.querySelector("#loadNote");
+  let storageCapability;
+  let noteName;
+  const content = document.querySelector("#content")
 
-    if(typeof(Storage) !== "undifined") {
-        //load
-        content.innerHTML = localStorage.getItem("content");
+  if (typeof Storage !== "undefined") {
+    //load
+    storageCapability = true;
+    content.innerHTML = localStorage.getItem("content");
+  }
 
-        //save
-        setInterval(function() {
-            localStorage.setItem("content", content.innerHTML);
-        }, 100);
-    } else {
-        alert("Your browser does not support web storage, your notes will not be saved.");
-    }
+saveNote.onclick = function () {
+  noteName = document.getElementById("noteName").value;
+  localStorage.setItem(noteName, content.innerHTML);
+  alert("Note Saved As " + noteName);
 }
+
+loadNote.onclick = function () {
+  noteName = document.getElementById("noteName").value;
+  if (storageCapability == true) {
+    content.innerHTML = localStorage.getItem(noteName)
+  } else {
+    alert("Error: Unable to access note")
+  };
+}
+
+    //save
+/*    setInterval(function() {
+      localStorage.setItem("content", content.innerHTML);
+    }, 100);
+  } else {
+    alert(
+      "Your browser does not support web storage, your notes will not be saved."
+    );
+  } */
+};
+
